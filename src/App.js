fedffa29bdedeb92c16a69be1b2f3d9a6eaf9a6e
Edward Osindo <eddu.almasi@gmail.com>
2020-08-16 09:29:21 +0300

    Todo-App component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem'
 
 function App() {
-        const firstname = "Edu"
-        const lastname = "Ted"
-      return (
-        <div className="App">
-            <h1>Hello {`${firstname} ${lastname}`} </h1>
-          <input type="checkbox" />
-          <p> Placeholder Text</p>
 
-          <input type="checkbox" />
-          <p> Placeholder Text</p>
+//        const date = new Date()
+//        const hours = date.getHours()
+//        let timeOfDay
+//        const styles = {
+//        fontSize: 30
+//        }
+//
+//        if (hours < 12) {
+//            timeOfDay = "Morning"
+//            styles.color = "#D90000"
+//        } else if (hours >=12 && hours < 17) {
+//            timeOfDay = "Afternoon"
+//            styles.color = "1475GF"
+//        } else {
+//            timeOfDay = "Evening"
+//            styles.color = "0A795F"
+//        }
+    return (
+        <div className="todo-list">
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
+          <TodoItem/>
 
-          <input type="checkbox" />
-          <p> Placeholder Text</p>
-
-          <input type="checkbox" />
-          <p> Placeholder Text</p>
         </div>
-      );
+
+//          <div>
+//            <h1 style ={styles}> Good {timeOfDay}</h1>
+//          </div>
+    );
 }
 
 export default App;
