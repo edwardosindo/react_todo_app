8c30fd8526acc1aef1d32ae5fb9a629d9c42664d
Edward Osindo <eddu.almasi@gmail.com>
2020-08-17 07:36:44 +0300

    Class based component form function

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import './App.css';
 import TodoItem from './components/TodoItem'
 import todosData from './components/todosData'
 
-function App() {
-    const todoItems = todosData.map(item => <TodoItem key={item.id} item={item} />)
-    return (
-        <div className="todo-list">
-          {todoItems}
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
 
-        </div>
-    );
+    render () {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        return (
+            <div className="todo-list">
+              {todoItems}
+
+            </div>
+        )
+    }
+
 }
 
 export default App;
